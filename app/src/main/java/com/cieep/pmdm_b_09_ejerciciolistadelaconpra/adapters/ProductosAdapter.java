1c43da6f1de3b49637aa0684c3ba8709a4ca38de
eduard <eduard@cieep.com>
2023-01-16 21:06:44 +0100

    mm

---
+++ app/src/main/java/com/cieep/pmdm_b_09_ejerciciolistadelaconpra/adapters/ProductosAdapter.java
@@ -0,0 +1,42 @@
+package com.cieep.pmdm_b_09_ejerciciolistadelaconpra.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cieep.pmdm_b_09_ejerciciolistadelaconpra.modelos.Producto;
+
+import java.util.List;
+
+public class ProductosAdapter extends RecyclerView.Adapter<ProductosAdapter.ProductoVH> {
+
+    private List<Producto> objects;
+    private int resource;
+    private Context context;
+
+    @NonNull
+    @Override
+    public ProductoVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductoVH holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ProductoVH extends RecyclerView.ViewHolder {
+        public ProductoVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
